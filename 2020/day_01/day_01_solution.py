4c87bc74c15e6785b08ad75f262a814f90ee4a58
Jordy Williams <jordy_williams@hotmail.co.uk>
2020-12-03 16:47:44 +0100

    feat: add typing to day 1

--- 2020/day_01/day_01_solution.py
+++ 2020/day_01/day_01_solution.py
@@ -1,6 +1,7 @@
 """ Solution to Day 01 of Advent of Code 2020 """
 
 from itertools import combinations
+import typing as ty
 from pathlib import Path
 import numpy as np
 
@@ -10,7 +11,9 @@ with open(Path.joinpath(DATA_PATH, "input.txt")) as input_file:
     DATA = [int(x) for x in input_file.readlines()]
 
 
-def get_numbers_that_sum(data, numbers_that_sum=2, target_sum=2020):
+def get_numbers_that_sum(
+    data: ty.List[int], numbers_that_sum: int = 2, target_sum: int = 2020
+) -> ty.List[int]:
     """ Returns a list of numbers_that_sum which sum to target_sum """
     return [
         comb for comb in combinations(data, numbers_that_sum) if sum(comb) == target_sum
