c312012c83e224d22fd3c1302547f6f4b0584187
Jordy Williams <jordy_williams@hotmail.co.uk>
2020-12-03 17:49:45 +0100

    feat: add day 3 solution

---
+++ 2020/day_03/day_03_solution.py
@@ -0,0 +1,50 @@
+""" Solution to Day 03 of Advent of Code 2020 """
+from pathlib import Path
+import typing as ty
+import numpy as np
+
+DATA_PATH = Path.resolve(Path(__file__).parent)
+
+""" Slopes to check --> PART 2
+    Right 1, down 1.
+    Right 3, down 1. Part 1
+    Right 5, down 1.
+    Right 7, down 1.
+    Right 1, down 2.
+"""
+
+PART_2_SLOPES = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+
+with open(Path.joinpath(DATA_PATH, "input.txt")) as input_file:
+    DATA = [
+        line.strip() for line in input_file.readlines()
+    ]  # each line will be one 'row' of map
+
+
+def count_number_trees_hit_in_path(
+    data: ty.List, num_right: int = 3, num_down: int = 1
+):
+    """Counts number of trees hit in a downward/right
+    traverse path.
+
+    Assumes all rows loop -> and Tree is # symbol
+    """
+    # Define start position in top left
+    right_start = down_start = num_trees = 0
+    while down_start < len(data):
+        num_trees += data[down_start][right_start % len(data[0])] == "#"
+        right_start += num_right
+        down_start += num_down
+    return num_trees
+
+
+PART_1_ANS = count_number_trees_hit_in_path(DATA)
+print(f"Part 1:\n{PART_1_ANS} 🎄 hit\n")
+
+PART_2_PER_SLOPE = [
+    count_number_trees_hit_in_path(DATA, slope[0], slope[1]) for slope in PART_2_SLOPES
+]
+PART_2_ANS = np.product(PART_2_PER_SLOPE)
+
+print(f"Part 2:\n{PART_2_PER_SLOPE} 🎄 hit for {PART_2_SLOPES}")
+print(f"Answer: {PART_2_ANS}\n")
