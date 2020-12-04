518764ad71560e57c5d215de2aa512f198514d67
Jordy Williams <jordy_williams@hotmail.co.uk>
2020-12-04 01:01:35 +0100

    feat: add timed results to day 1

--- 2020/day_01/day_01_solution.py
+++ 2020/day_01/day_01_solution.py
@@ -3,6 +3,7 @@
 from itertools import combinations
 import typing as ty
 from pathlib import Path
+import timeit
 import numpy as np
 
 DATA_PATH = Path.resolve(Path(__file__).parent)
@@ -20,11 +21,17 @@ def get_numbers_that_sum(
     ]
 
 
+PART_1_START_TIME = timeit.default_timer()
 PART_1_INTS_TO_SUM = get_numbers_that_sum(DATA)
 PART_1_ANS = [np.product(comb) for comb in PART_1_INTS_TO_SUM]
+PART_1_TIME_MS = (timeit.default_timer() - PART_1_START_TIME) * 1000
 
+PART_2_START_TIME = timeit.default_timer()
 PART_2_INTS_TO_SUM = get_numbers_that_sum(DATA, 3)
 PART_2_ANS = [np.product(comb) for comb in PART_2_INTS_TO_SUM]
+PART_2_TIME_MS = (timeit.default_timer() - PART_2_START_TIME) * 1000
 
 print(f"Part 1:\nInts that sum are: {PART_1_INTS_TO_SUM}\nProduct is: {PART_1_ANS}\n")
 print(f"Part 2:\nInts that sum are: {PART_2_INTS_TO_SUM}\nProduct is: {PART_2_ANS}\n")
+
+print(f"Timed Results:\nPart 1: {PART_1_TIME_MS} ms\nPart 2: {PART_2_TIME_MS} ms\n")
