06ed53a4491e1650d88d353f2208103b0e6cf3bb
Jordy Williams <jordy_williams@hotmail.co.uk>
2020-12-05 09:48:31 +0100

    feat: add day 05 solutions

---
+++ 2020/day_05/day_05_solution.py
@@ -0,0 +1,54 @@
+""" Solution to Day 05 of Advent of Code 2020 """
+from pathlib import Path
+import typing as ty
+import timeit
+
+# Constants
+DATA_PATH = Path.resolve(Path(__file__).parent)
+# use str values for string mapping
+BINARY_MAPPING = {"F": "0", "B": "1", "L": "0", "R": "1"}
+RANGES = {"row": 127, "column": 7}
+ROW_DATA = ["F", "B"]
+COL_DATA = ["R", "L"]
+
+with open(Path.joinpath(DATA_PATH, "input.txt")) as input_file:
+    DATA = input_file.readlines()
+
+# Data analysis functions - binary conversion
+def get_row_and_column(seat_str: str) -> int:
+    """Gets row and column from seat data is binary int"""
+    seat_str = seat_str.translate(str.maketrans(BINARY_MAPPING))
+    return int(seat_str, 2)
+
+
+def get_seat_id(all_ids: ty.List[int]) -> int:
+    """Gets missing ID from list of all IDs
+    Assumes only one missing number returns None no seat
+    """
+    assigned_seat_id = None
+    for seat_id in all_ids:
+        # as all binary numbers until max range should exist (apart from very front)
+        # numbers either side of current ID should exist, and not the one above
+        target_id = seat_id + 1
+        if target_id + 1 in all_ids and not target_id in all_ids:
+            assigned_seat_id = target_id
+            break
+    return assigned_seat_id
+
+
+# Answers
+PART_1_START_TIME = timeit.default_timer()
+ALL_SEAT_IDS = [get_row_and_column(seat) for seat in DATA]
+PART_1_ANS = max(ALL_SEAT_IDS)
+PART_1_TIME_MS = (timeit.default_timer() - PART_1_START_TIME) * 1000
+
+PART_2_START_TIME = timeit.default_timer()
+PART_2_ANS = get_seat_id(ALL_SEAT_IDS)
+PART_2_TIME_MS = (timeit.default_timer() - PART_2_START_TIME) * 1000
+
+
+print(f"Part 1:\n{PART_1_ANS} 🛫 max seat number\n")
+print(f"Part 2:\n{PART_2_ANS} 🛫 seat ID\n")
+print(
+    f"Timed Results:\nPart 1: {PART_1_TIME_MS:.3f} ms\nPart 2: {PART_2_TIME_MS:.3f} ms\n"
+)
