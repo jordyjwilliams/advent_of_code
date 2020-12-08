798cd59d5c4a68c751b007494346645871d5366b
Jordy Williams <jordy_williams@hotmail.co.uk>
2020-12-08 10:47:20 +0100

    feat: add template dir

---
+++ 2020/day_xx/README.md
@@ -0,0 +1,8 @@
+## Template directory for `day_xx` AOC solutions
+ 
+### How to use:
+* copy directory, and add `input.txt` file
+* replace `xx` with day number
+
+### Contains
+* standard boilerplate timing and printing code
---
+++ 2020/day_xx/day_xx_solution.py
@@ -0,0 +1,34 @@
+""" Solution to Day XX of Advent of Code 2020 """
+from pathlib import Path
+import timeit
+
+# Constants
+DATA_PATH = Path.resolve(Path(__file__).parent)
+
+# read data line by line
+with open(Path.joinpath(DATA_PATH, "input.txt")) as input_file:
+    DATA = input_file.readlines()
+
+
+# Data parsing functions
+# DEFINE HERE...
+
+
+# Data analysis
+# DEFINE HERE...
+
+# Answers
+PART_1_START_TIME = timeit.default_timer()
+PART_1_ANS = "CALCULATE THIS"
+PART_1_TIME_MS = (timeit.default_timer() - PART_1_START_TIME) * 1000
+
+PART_2_START_TIME = timeit.default_timer()
+PART_2_ANS = "CALCULATE THIS"
+PART_2_TIME_MS = (timeit.default_timer() - PART_2_START_TIME) * 1000
+
+print(f"Part 1:\n{PART_1_ANS}\n")
+print(f"Part 2:\n{PART_2_ANS}\n")
+
+print(
+    f"Timed Results:\nPart 1: {PART_1_TIME_MS:.3f} ms\nPart 2: {PART_2_TIME_MS:.3f} ms\n"
+)
