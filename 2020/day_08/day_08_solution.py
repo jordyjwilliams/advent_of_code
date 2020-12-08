52a83f2332875f7b7bb1622bcba74f2dfcc71729
Jordy Williams <jordy_williams@hotmail.co.uk>
2020-12-08 10:46:12 +0100

    feat: add day 08 solutions

---
+++ 2020/day_08/day_08_solution.py
@@ -0,0 +1,93 @@
+""" Solution to Day 08 of Advent of Code 2020 """
+from pathlib import Path
+import typing as ty
+import timeit
+
+# Constants
+DATA_PATH = Path.resolve(Path(__file__).parent)
+OPERATIONS = ["acc", "jmp", "nop"]
+
+with open(Path.joinpath(DATA_PATH, "input.txt")) as input_file:
+    DATA = input_file.readlines()
+
+
+# Data parsing functions
+def run_instruction(
+    instruction: str, current_idx: int, arg: int, accumulator: int
+) -> ty.Tuple[int, int]:
+    """run instruction given by string
+    returns modified current_idx, accumulator dependent on instruction
+    raises ValueError if instruction not supported
+    """
+    if instruction not in OPERATIONS:
+        raise ValueError(f"Instruction must be one of {OPERATIONS}")
+    if instruction in ["acc", "nop"]:
+        current_idx += 1
+        if instruction == "acc":
+            accumulator += arg
+    elif instruction == "jmp":
+        current_idx += arg
+    return current_idx, accumulator
+
+
+def run_loop(data: ty.List[str]) -> ty.Tuple[int, int, bool]:
+    """Run loop for list of boot code strings
+    Will run until:
+        * complete (run idx same as length of data)
+        * or the same instruction is ran twice (boot loop)
+    Returns
+    accumulator, current_index, complete_boot_loop
+    """
+    accumulator = 0
+    current_idx = 0
+    lines_ran = []
+    while True:
+        if current_idx in lines_ran:
+            complete_boot_loop = False
+            return accumulator, current_idx, complete_boot_loop
+        if current_idx == len(data):
+            complete_boot_loop = True
+            return accumulator, current_idx, complete_boot_loop
+        line = data[current_idx]
+        func_key, arg = line.split(" ")
+        arg = int(arg)
+        lines_ran.append(current_idx)
+        current_idx, accumulator = run_instruction(
+            func_key, current_idx, arg, accumulator
+        )
+
+
+# Answers
+PART_1_START_TIME = timeit.default_timer()
+
+PART_1_ANS, BREAKS_AT_IDX, _ = run_loop(DATA)
+PART_1_TIME_MS = (timeit.default_timer() - PART_1_START_TIME) * 1000
+
+PART_2_START_TIME = timeit.default_timer()
+LINE_IDXS_TO_SWITCH = [
+    idx for idx, line in enumerate(DATA) if "nop" in line or "jmp" in line
+]
+
+for idx in LINE_IDXS_TO_SWITCH:
+    NEW_DATA = list(DATA)
+    NEW_DATA[idx] = (
+        NEW_DATA[idx].replace("nop", "jmp")
+        if "nop" in NEW_DATA[idx]
+        else NEW_DATA[idx].replace("jmp", "nop")
+    )
+    ACC, _, COMPLETE = run_loop(NEW_DATA)
+    if COMPLETE:
+        MODIFY_INDEX = idx
+        break
+
+PART_2_ANS = ACC
+PART_2_TIME_MS = (timeit.default_timer() - PART_2_START_TIME) * 1000
+
+print(
+    f"Part 1:\n{PART_1_ANS} accumulator value. Idx: {BREAKS_AT_IDX} causes inf loop\n"
+)
+print(f"Part 2:\n{PART_2_ANS} accumulator value. Changing idx: {MODIFY_INDEX} fixes\n")
+
+print(
+    f"Timed Results:\nPart 1: {PART_1_TIME_MS:.3f} ms\nPart 2: {PART_2_TIME_MS:.3f} ms\n"
+)
