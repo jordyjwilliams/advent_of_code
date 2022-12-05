4c1a85314b40a1db532bb9a2ad052b1d0bcdc94d
Jordy Williams <jordy_williams@hotmail.co.uk>
2022-12-06 10:02:26 +1100

    style: nicer `day4` printing and begin cleanup. Adds `TODO`

--- 2022/day_04/day_04_solution.py
+++ 2022/day_04/day_04_solution.py
@@ -1,6 +1,5 @@
 """ Solution to Day 04 of Advent of Code 2022 """
 import timeit
-import typing as ty
 from pathlib import Path
 
 # Constants
@@ -11,24 +10,27 @@ with open(DATA_PATH / "input.txt", encoding="UTF-8") as input_file:
     DATA = input_file.readlines()
 
 
-def parse_dataline_to_containment(data: str, part2: bool = False) -> ty.List[str]:
-    """Priority for item which matches in both compartments of bag score for data line, returns list of integers.
+def parse_dataline_to_containment(data: str, part2: bool = False) -> int:
+    """Elf pair datalines. Returns number of contained/overlapping datasets.
 
     Parameters
     ----------
     data : str
-        input data of bag contents. Each compartment is of equal length.
+        elf pair data AA-BB,XX-YY format
+        data will be strings of ints
     part2 : bool, optional
         if True part 2 solution
 
     Returns
     -------
-    priorities : list[int]
-        list of scores for each line
+    contained : int
+        number contained as specified in question
     """
     contained = 0
     for line in data:
         range_data = [[int(num) for num in itm.split("-")] for itm in line.split(",")]
+        # part one, one range fully contains another
+        ## TODO: cleanup this logic
         if (
             range_data[0][0] >= range_data[1][0]
             and range_data[0][1] <= range_data[1][1]
@@ -37,6 +39,8 @@ def parse_dataline_to_containment(data: str, part2: bool = False) -> ty.List[str
             and not part2
         ):
             contained += 1
+        # part two, assignment pair ranges overlap (at all)
+        ## TODO: invoke sets in nicer way.
         elif (
             set(range(range_data[0][0], range_data[0][1] + 1)).intersection(
                 set(range(range_data[1][0], range_data[1][1] + 1))
@@ -46,9 +50,6 @@ def parse_dataline_to_containment(data: str, part2: bool = False) -> ty.List[str
             contained += 1
     return contained
 
-
-# Data analysis
-# DEFINE HERE...
 
 # Answers
 PART_1_START_TIME = timeit.default_timer()
@@ -60,7 +61,7 @@ PART_2_ANS = parse_dataline_to_containment(DATA, part2=True)
 PART_2_TIME_MS = (timeit.default_timer() - PART_2_START_TIME) * 1000
 
 if __name__ == "__main__":
-    print(f"{PART_1_ANS=}\n")
-    print(f"{PART_2_ANS=}\n")
+    print(f"Number assignment pairs fully contained: {PART_1_ANS} pairs\n")
+    print(f"Number assignment pairs with overlapping ranges: {PART_2_ANS} pairs\n")
 
     print(f"Timed Results:\n{PART_1_TIME_MS=:.3f}\n{PART_2_TIME_MS=:.3f}\n")
